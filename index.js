693bfdbeb55e7721328db605cd4378f12b46b1ae
jimmyma <jimmyma@croxlink.com>
2019-08-29 16:03:14 +0800

    初始化api router

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const jimmy_assistant = require('./project_list/jimmy_assistant/index')
-const Ptt_img_crawler = require('./api/ptt_beauty/ptt_beauty_crawler')
+const Ptt_img_crawler = require('./beauty_api')
 
 const app = express();
 
@@ -8,20 +7,18 @@ app.get('/', (req, res) => {
   res.send('wellcome my channel，此為chat bot的測試首頁!')
 });
 
-const pretty_api = () => {
-  const ptt_img_crawler = new Ptt_img_crawler({
-    url_index: 2000,
-    page_index: 0,
-    push: 0
+// 表特版api接口，可想更多參數
+app.get('/ptt_beauty/:url_index/:page_index/:push', (req, res) => {
+  // 用這邊的方法取得express內建的參數讀取
+  console.log(req.params)
+  // 或用這邊的方法解析參數
+  console.log(req.url)
+
+  let ptt_img_crawler = new Ptt_img_crawler(req.params)
+  ptt_img_crawler.beauty_image_api().then((article) => {
+    console.log('測試用log：', article)
+    res.send(article)
   })
-  return ptt_img_crawler.beauty_image_api().then((article) => {
-    return article
-  }).catch(() => {})
-
-}
-
-app.get('/pretty', (req, res) => {
-  res.send('wellcome my channel，此為chat bot的測試首頁!')
 });
 
 var server = app.listen(process.env.PORT || 8080, function () {
